fa68bad11b28bde6b8647674f3e5da41b7a44d2d
bradleywjenks <b.jenks21@imperial.ac.uk>
2024-10-15 18:45:27 +0100

    manual data processing updates

--- scripts/process-data.py
+++ scripts/process-data.py
@@ -23,13 +23,13 @@ args = parser.parse_args()
 # get good data periods
 
 calibration_data = pd.read_excel(TIMESERIES_DIR / 'raw/metrinet-calibration-records.xlsx')
-if 'datetime' not in calibration_data.columns:
+if 'good_datetime' not in calibration_data.columns:
    raise ValueError("The specified file must contain a 'datetime' column.")
-calibration_dates = pd.Series(calibration_data['datetime'].dt.to_period('D').dt.to_timestamp().unique()).sort_values()
+calibration_dates = pd.Series(calibration_data['good_datetime'].dt.to_period('D').dt.to_timestamp().unique()).sort_values()
 
 good_data = {}
 for idx, cal_date in calibration_dates.items():
-   start_date = cal_date + pd.Timedelta(days=1)
+   start_date = cal_date
    end_date = start_date + pd.Timedelta(days=args.n_days-1, hours=23, minutes=45, seconds=0)
 
    good_data[idx+1] = { 
@@ -37,10 +37,13 @@ for idx, cal_date in calibration_dates.items():
        'end_date': end_date
    }
 
+
+print(good_data)
+
 keys_to_delete = []
 for idx, cal_date in calibration_dates.items():
    for j in good_data.keys():
-       if good_data[j]['start_date'] <= cal_date <= good_data[j]['end_date']:
+       if good_data[j]['start_date'] < cal_date < good_data[j]['end_date']:
            if j not in keys_to_delete:
                print(f'Note! Good data period {j} violated by calibration date {cal_date}. Good data period removed.')
                keys_to_delete.append(j)
@@ -74,13 +77,88 @@ pressure_df['datetime'] = pd.to_datetime(pressure_df['datetime'])
 flow_df['datetime'] = pd.to_datetime(flow_df['datetime'])
 wq_df['datetime'] = pd.to_datetime(wq_df['datetime'])
 
+
+
+
 # process time series data
 output_dir = TIMESERIES_DIR / 'processed/'
 
 for idx, period in good_data.items():
+
    filtered_pressure_df = pressure_df[(pressure_df['datetime'] >= period['start_date']) & (pressure_df['datetime'] <= period['end_date'])]
    filtered_flow_df = flow_df[(flow_df['datetime'] >= period['start_date']) & (flow_df['datetime'] <= period['end_date'])]
    filtered_wq_df = wq_df[(wq_df['datetime'] >= period['start_date']) & (wq_df['datetime'] <= period['end_date'])]
+
+   # hard-coded chlorine data modifications...
+   if idx == 4:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_2', 'chlorine'] = np.nan
+   elif idx == 7:
+      filtered_pressure_df = pressure_df[(pressure_df['datetime'] >= period['start_date']) & (pressure_df['datetime'] <= pd.to_datetime('2023-01-29 06:00:00'))]
+      filtered_flow_df = flow_df[(flow_df['datetime'] >= period['start_date']) & (flow_df['datetime'] <= pd.to_datetime('2023-01-29 06:00:00'))]
+      filtered_wq_df = wq_df[(wq_df['datetime'] >= period['start_date']) & (wq_df['datetime'] <= pd.to_datetime('2023-01-29 06:00:00'))]
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+   elif idx == 8:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_1', 'chlorine'] = np.nan
+   elif idx == 9:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+   elif idx == 10:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+   elif idx == 11:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+   elif idx == 12:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW9_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW4_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+   elif idx == 13:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW9_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW4_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[(filtered_wq_df['bwfl_id'] == 'BW2') & (filtered_wq_df['datetime'] >= pd.to_datetime('2024-04-04 11:00:00')), 'chlorine'] = np.nan
+   elif idx == 14:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW9_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW4_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+   elif idx == 15:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW9_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW4_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW3', 'chlorine'] = np.nan
+      filtered_wq_df.loc[(filtered_wq_df['bwfl_id'] == 'BW7') & (filtered_wq_df['datetime'] >= pd.to_datetime('2024-05-31 08:30:00')) & (filtered_wq_df['datetime'] <= pd.to_datetime('2024-06-03 09:45:00')), 'chlorine'] = np.nan
+      filtered_wq_df.loc[(filtered_wq_df['bwfl_id'] == 'BW6') & (filtered_wq_df['datetime'] <= pd.to_datetime('2024-06-03 23:15:00')), 'chlorine'] = np.nan
+      filtered_wq_df.loc[(filtered_wq_df['bwfl_id'] == 'BW5') & (filtered_wq_df['datetime'] >= pd.to_datetime('2024-06-03 08:45:00')), 'chlorine'] = np.nan
+   elif idx == 16:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW9_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW4_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW5', 'chlorine'] = np.nan
+   elif idx == 17:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW9_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW4_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_1', 'chlorine'] = np.nan
+   elif idx == 18:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW9_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW4_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_1', 'chlorine'] = np.nan
+   elif idx == 19:
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW9_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW4_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW12_1', 'chlorine'] = np.nan
+      filtered_wq_df.loc[filtered_wq_df['bwfl_id'] == 'BW1_2', 'chlorine'] = np.nan
+      filtered_wq_df.loc[(filtered_wq_df['bwfl_id'] == 'BW7') & (filtered_wq_df['datetime'] >= pd.to_datetime('2024-09-26 17:30:00')), 'chlorine'] = np.nan
+
+   # export to csv files
    if not filtered_wq_df.empty:
       filtered_pressure_df.to_csv(output_dir / f"{str(idx).zfill(2)}-pressure.csv", index=False)
       filtered_flow_df.to_csv(output_dir / f"{str(idx).zfill(2)}-flow.csv", index=False)
