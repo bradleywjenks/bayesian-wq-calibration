1e3dc715e73021d700af9ef3070cb277ff71c089
bradleywjenks <b.jenks21@imperial.ac.uk>
2024-12-21 19:58:07 +0000

    modified design of experiments function for training gaussian process model

--- bayesian_wq_calibration/calibration.py
+++ bayesian_wq_calibration/calibration.py
@@ -4,6 +4,14 @@ import pandas as pd
 import numpy as np
 import random
 from deap import base, creator, tools, algorithms
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.colors
+default_colors = plotly.colors.qualitative.Plotly
+from scipy.stats.qmc import Sobol, Halton
+from scipy.stats import norm, truncnorm, triang, uniform
+import random
+from pyDOE import lhs
 
 
 
@@ -120,7 +128,90 @@ def fitness(wn, cl_df, grouping, wall_coeffs, obj_function):
 """
 Statistical emulator/surrogate modelling functions.
 """
-# insert functions here...
+
+def generate_samples(param_mean, param_bounds, param_group, n_samples, sampling_method='lhs', dist_type='truncated normal', rel_uncertainty=0.5, plot=False):
+
+    n_params = len(param_mean)
+
+    if sampling_method == 'lhs':
+        samples = lhs(n_params, samples=n_samples)
+    elif sampling_method == 'monte carlo':
+        samples = np.random.rand(n_samples, n_params)
+    elif sampling_method == 'sobol':
+        sampler = Sobol(d=n_params, scramble=True)
+        samples = sampler.random_base2(m=int(np.log2(n_samples)))
+    elif sampling_method == 'halton':
+        sampler = Halton(d=n_params, scramble=True)
+        samples = sampler.random(n_samples)
+    else:
+        raise ValueError(f"Unsupported sampling method: {sampling_method}")
+    
+    scaled_samples = []
+
+    if plot:
+        cols = 2
+        rows = (n_params + cols - 1) // cols
+        fig = make_subplots(rows=rows, cols=cols, subplot_titles=param_group, horizontal_spacing=0.2)
+
+    for i, sample in enumerate(samples.T):
+        mu = param_mean[i]
+        if mu == 0:
+            mu = -1e-4
+        lower_bound, upper_bound = param_bounds[i]
+
+        if dist_type == 'truncated normal':
+            sigma = abs(mu * rel_uncertainty)
+            a, b = (lower_bound - mu) / sigma, (upper_bound - mu) / sigma # upper bound is 0
+            param_samples = truncnorm.ppf(sample, a=a, b=b, loc=mu, scale=sigma)
+
+            if plot:
+                x = np.linspace(lower_bound, 0, 500)
+                y = truncnorm.pdf(x, a=a, b=b, loc=mu, scale=sigma)
+
+        elif dist_type == 'triangle':
+            c = (mu - lower_bound) / (upper_bound - lower_bound) # mean is the mode
+            param_samples = triang.ppf(sample, c=c, loc=lower_bound, scale=upper_bound - lower_bound)
+
+            if plot:
+                x = np.linspace(lower_bound, upper_bound, 500)
+                y = triang.pdf(x, c=c, loc=lower_bound, scale=upper_bound - lower_bound)
+
+        elif dist_type == 'uniform':
+            param_samples = uniform.ppf(sample, loc=lower_bound, scale=upper_bound - lower_bound) # uniform between bounds
+
+            if plot:
+                x = np.linspace(lower_bound, upper_bound, 500)
+                y = uniform.pdf(x, loc=lower_bound, scale=upper_bound - lower_bound)
+
+        else:
+            raise ValueError(f"Unsupported distribution type: {dist_type}")
+
+        if plot:
+            row, col = divmod(i, cols)
+            fig.add_trace(
+                go.Scatter(x=x, y=y, mode='lines', line=dict(dash='solid', color='grey'), name=f'{param_group[i]} pdf'),
+                row=row + 1, col=col + 1
+            )
+            fig.add_trace(
+                go.Scatter(x=param_samples, y=[0] * len(param_samples), mode='markers',
+                           marker=dict(size=6, color=default_colors[0], opacity=0.8), name=f'{param_group[i]} samples'),
+                row=row + 1, col=col + 1
+            )
+            fig.update_xaxes(title_text="decay coefficient [m/d]", row=row + 1, col=col + 1)
+            fig.update_yaxes(title_text="density", row=row + 1, col=col + 1)
+
+        scaled_samples.append(param_samples)
+
+    if plot:
+        fig.update_layout(
+            template="simple_white",
+            width=650,
+            height=300 * rows,
+            showlegend=False
+        )
+        fig.show()
+
+    return np.array(scaled_samples).T
 
 
 
